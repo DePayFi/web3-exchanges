76c6dd40f793d1e6c8840cede0f51bd047fc7313
Sebastian Pape <s@pape.io>
2021-07-15 14:50:31 +0200

    1.1.5: fix yarn dev and built

--- dist/es/index.js
+++ dist/es/index.js
@@ -1,3 +1,5 @@
+
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 import { Token } from 'depay-blockchain-token';
 import CONSTANTS from 'depay-blockchain-constants';
 import { request } from 'depay-blockchain-client';
