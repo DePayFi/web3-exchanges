b94d867a8d1fbace9c446e47d63fc33a5951ec6a
Sebastian Pape <s@pape.io>
2022-10-30 13:52:47 +0100

    v11.2.2: fix evm build

--- src/all.evm.js
+++ src/all.evm.js
@@ -1,7 +1,7 @@
-import pancakeswap from './exchanges/pancakeswap'
-import quickswap from './exchanges/quickswap'
-import uniswap_v2 from './exchanges/uniswap_v2'
-import wagyuswap from './exchanges/wagyuswap'
+import pancakeswap from './exchanges/pancakeswap/index.evm'
+import quickswap from './exchanges/quickswap/index.evm'
+import uniswap_v2 from './exchanges/uniswap_v2/index.evm'
+import wagyuswap from './exchanges/wagyuswap/index.evm'
 
 let all = {
   ethereum: [uniswap_v2],
