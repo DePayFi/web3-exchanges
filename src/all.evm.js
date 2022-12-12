92d3be16271b9c1133ed4b6b41335930e74dcb66
Sebastian Pape <s@pape.io>
2022-12-12 16:40:40 +0100

    v11.3.3: fix evm build

--- src/all.evm.js
+++ src/all.evm.js
@@ -2,10 +2,10 @@ import pancakeswap from './exchanges/pancakeswap/index.evm'
 import quickswap from './exchanges/quickswap/index.evm'
 import uniswap_v2 from './exchanges/uniswap_v2/index.evm'
 import wagyuswap from './exchanges/wagyuswap/index.evm'
-import wbnb from './exchanges/wbnb'
-import weth from './exchanges/weth'
-import wmatic from './exchanges/wmatic'
-import wvlx from './exchanges/wvlx'
+import wbnb from './exchanges/wbnb/index.evm'
+import weth from './exchanges/weth/index.evm'
+import wmatic from './exchanges/wmatic/index.evm'
+import wvlx from './exchanges/wvlx/index.evm'
 
 let all = {
   ethereum: [uniswap_v2, weth],
---
+++ src/exchanges/weth/index.evm.js
@@ -0,0 +1,13 @@
+import basics from './basics'
+import Exchange from '../../classes/Exchange.evm'
+import { findPath } from './route/path'
+import { getAmounts } from './route/amounts'
+import { getTransaction } from './route/transaction'
+
+export default new Exchange(
+  Object.assign(basics, {
+    findPath,
+    getAmounts,
+    getTransaction,
+  })
+)
