e75cedd21f98d1eff9696094d7e28e48ee7ac1d6
Sebastian Pape <0xneo11@gmail.com>
2024-12-26 21:41:30 +0100

    v13.15.1: fixes uniswapV3 liquidity check for smaller amounts

--- dist/umd/index.evm.js
+++ dist/umd/index.evm.js
@@ -2089,14 +2089,14 @@
             const amountScaled = await getOutputAmount({ exchange, pool, inputAmount: ethers.ethers.BigNumber.from(amountIn).mul(ethers.ethers.BigNumber.from(10)).toString() });
             const amountScaledDown = amountScaled.div(ethers.ethers.BigNumber.from(10));
             const difference = amountScaledDown.sub(amount).abs();
-            const enoughLiquidity = !difference.gt(amount.div(ethers.ethers.BigNumber.from(100)).mul(ethers.ethers.BigNumber.from(5))); // up to 5% diff allowed
+            const enoughLiquidity = !difference.gt(amount.mul(ethers.ethers.BigNumber.from(5)).div(ethers.ethers.BigNumber.from(100))); // up to 5% diff allowed
             if(!enoughLiquidity) { return }
           } else {
             amount = await getInputAmount({ exchange, pool, outputAmount: amountOut });
             const amountScaled = await getInputAmount({ exchange, pool, outputAmount: ethers.ethers.BigNumber.from(amountOut).mul(ethers.ethers.BigNumber.from(10)).toString() });
             const amountScaledDown = amountScaled.div(ethers.ethers.BigNumber.from(10));
             const difference = amountScaledDown.sub(amount).abs();
-            const enoughLiquidity = !difference.gt(amount.div(ethers.ethers.BigNumber.from(100)).mul(ethers.ethers.BigNumber.from(5))); // up to 5% diff allowed
+            const enoughLiquidity = !difference.gt(amount.mul(ethers.ethers.BigNumber.from(5)).div(ethers.ethers.BigNumber.from(100))); // up to 5% diff allowed
             if(!enoughLiquidity) { return }
           }
 
--- src/platforms/evm/uniswap_v3.js
+++ src/platforms/evm/uniswap_v3.js
@@ -119,14 +119,14 @@ const getBestPool = async ({ blockchain, exchange, path, amountIn, amountOut, bl
           const amountScaled = await getOutputAmount({ exchange, pool, inputAmount: ethers.BigNumber.from(amountIn).mul(ethers.BigNumber.from(10)).toString() })
           const amountScaledDown = amountScaled.div(ethers.BigNumber.from(10))
           const difference = amountScaledDown.sub(amount).abs()
-          const enoughLiquidity = !difference.gt(amount.div(ethers.BigNumber.from(100)).mul(ethers.BigNumber.from(5))) // up to 5% diff allowed
+          const enoughLiquidity = !difference.gt(amount.mul(ethers.BigNumber.from(5)).div(ethers.BigNumber.from(100))) // up to 5% diff allowed
           if(!enoughLiquidity) { return }
         } else {
           amount = await getInputAmount({ exchange, pool, outputAmount: amountOut })
           const amountScaled = await getInputAmount({ exchange, pool, outputAmount: ethers.BigNumber.from(amountOut).mul(ethers.BigNumber.from(10)).toString() })
           const amountScaledDown = amountScaled.div(ethers.BigNumber.from(10))
           const difference = amountScaledDown.sub(amount).abs()
-          const enoughLiquidity = !difference.gt(amount.div(ethers.BigNumber.from(100)).mul(ethers.BigNumber.from(5))) // up to 5% diff allowed
+          const enoughLiquidity = !difference.gt(amount.mul(ethers.BigNumber.from(5)).div(ethers.BigNumber.from(100))) // up to 5% diff allowed
           if(!enoughLiquidity) { return }
         }
 
